1e86547c32e938e5e3d7168f08250c5d6aad18ee
FireflySky <51427307+FireflySky@users.noreply.github.com>
2019-07-11 08:50:23 +0800

    最终完成版

--- CH_7_11/js/Task.js
+++ CH_7_11/js/Task.js
@@ -1,21 +1,35 @@
 window.onload = () => {
-    var date = 3597;
-    var redate = 0;
+    var date = 0;
     var s="";//秒
     var m="";//分钟
     var h="";//小时
+    var setinter=null;
     document.getElementById("start").onclick = function () {
-        var setinter = setInterval(() => {
-            date++;
-            setDate(date);
-            document.getElementById("date").innerHTML=h+":"+m+":"+s;
-        }, 1000);
+        var val=this.innerHTML;
+        if(val=="开始"){
+            this.innerHTML="暂停";
+            setinter = setInterval(() => {
+                date++;
+                setDate(date);
+                document.getElementById("date").innerHTML=h+":"+m+":"+s;
+            }, 1000);
+        }else{
+            this.innerHTML="开始";
+            clearInterval(setinter);
+        }
+        
     }
 
+    //重置
+    document.getElementById("reset").onclick=()=>{
+        document.getElementById("date").innerHTML="00:00:00";
+        date=0;
+    };
+
+    //将秒数改为格式化的时间
     function setDate(date){
-       
         let H=Math.floor(date/3600);
-        let  M=Math.floor((date%3600)/60);
+        let M=Math.floor((date%3600)/60);
         let S=Math.floor((date%3600)%60);
         //小时
         if(H<1){
@@ -41,6 +55,5 @@ window.onload = () => {
         }else{
             s=S;
         }
-
     }
 }
